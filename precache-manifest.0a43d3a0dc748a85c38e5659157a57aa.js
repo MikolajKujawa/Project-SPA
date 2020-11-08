1a1df6162bca950cb7db1fe4fc6c074c26b5c83f
Mikołaj Kujawa <mkkujawix@gmail.com>
2020-11-08 18:37:58 +0100

    Updates

---
+++ precache-manifest.0a43d3a0dc748a85c38e5659157a57aa.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "55d2df79f255fa85404b",
+    "url": "/Project-SPA/static/css/main.7bde3b59.chunk.css"
+  },
+  {
+    "revision": "55d2df79f255fa85404b",
+    "url": "/Project-SPA/static/js/main.b083984a.chunk.js"
+  },
+  {
+    "revision": "78684f008a10ea80a397",
+    "url": "/Project-SPA/static/js/runtime~main.f0b5b449.js"
+  },
+  {
+    "revision": "10b595924a9112108439",
+    "url": "/Project-SPA/static/js/2.5695411f.chunk.js"
+  },
+  {
+    "revision": "70e6670ee746fc59f414fde4fcce93af",
+    "url": "/Project-SPA/static/media/header1.70e6670e.jpg"
+  },
+  {
+    "revision": "d9d36b65a132d26aed884ad58e4719eb",
+    "url": "/Project-SPA/static/media/header3.d9d36b65.jpg"
+  },
+  {
+    "revision": "33eca35fceea8311d67358a6629fa9d9",
+    "url": "/Project-SPA/static/media/header2.33eca35f.jpg"
+  },
+  {
+    "revision": "ad1d3853077ce71d66f9284b1ba70bf2",
+    "url": "/Project-SPA/index.html"
+  }
+];
